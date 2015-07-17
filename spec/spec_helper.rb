d476028ff6207cd9f16c1cfacdeaafdd7c685dde
Tomoya Chiba <tomo.asleep@gmail.com>
2015-07-17 13:44:12 +0900

    Drop uselessly caching of fixture files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,7 @@ require 'slacken'
 require 'helpers/document_component_dsl'
 
 def fixture(name)
-  @fixture_cache ||= {}
-  @fixture_cache[name] ||= File.read(File.expand_path(name, File.expand_path('fixtures', __dir__)))
+  File.read(File.expand_path(name, File.expand_path('fixtures', __dir__)))
 end
 
 RSpec.configure do |c|
