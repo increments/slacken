107ee765b6e0a692c0fc51c309fb073921e804c9
Tomoya Chiba <tomo.asleep@gmail.com>
2015-07-23 18:51:45 +0900

    Add a description of `scripts/update_markup_fixture.rb`

--- spec/slacken_spec.rb
+++ spec/slacken_spec.rb
@@ -5,6 +5,9 @@ describe Slacken do
     subject { described_class.translate(source) }
     let(:source) { fixture('example.html') }
 
+    # This test checks whether the behavior of this translation engine is unexpectedly broken.
+    # If you change the behavior, you should run `scripts/update_markup_fixture.rb`
+    # to update fixture file.
     it 'translates as expected' do
       expect(subject).to eq(fixture('markup_text.txt'))
     end
