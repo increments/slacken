6116a20e44201b6df30cf4019dbea117d557ecd7
Yuku TAKAHASHI <taka84u9@gmail.com>
2015-08-17 12:01:38 +0900

    Refactor DomContainer

--- lib/slacken.rb
+++ lib/slacken.rb
@@ -15,18 +15,14 @@ module Slacken
     #
     # Returns a markdown String.
     def translate(html_source)
-      if (component = convert_html_to_document_component(html_source))
-        component.to_element.to_s
-      else
-        ''
-      end
+      convert_html_to_document_component(html_source).to_element.to_s
     end
 
     private
 
     # Internal: Parse a HTML string and convert it to a DocumentComponent object.
     #
-    # Returns a DocumentComponent or nil.
+    # Returns a DocumentComponent.
     def convert_html_to_document_component(html_source)
       DocumentComponent.build_by_html(html_source)
     end
