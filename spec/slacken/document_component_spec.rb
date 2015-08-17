c9906010336a4f198f9edb91a2c142f34804d620
Yuku TAKAHASHI <taka84u9@gmail.com>
2015-08-17 12:16:57 +0900

    Tiny code cleaning

--- spec/slacken/document_component_spec.rb
+++ spec/slacken/document_component_spec.rb
@@ -1,18 +1,16 @@
 require 'spec_helper'
 
-module Slacken
-  describe DocumentComponent do
-    let(:document_component) { described_class.build_by_html(source) }
-    let(:source) { fixture('example.html') }
+describe Slacken::DocumentComponent do
+  let(:document_component) { described_class.build_by_html(source) }
+  let(:source) { fixture('example.html') }
 
-    describe '#normalize' do
-      subject { document_component.normalize }
+  describe '#normalize' do
+    subject { document_component.normalize }
 
-      DocumentComponent::NORMALIZE_FILTERS.each do |klass|
-        context "when #{klass.name} checks the result's validity" do
-          let(:filter) { klass.new }
-          it { is_expected.to satisfy(&filter.method(:valid?)) }
-        end
+    Slacken::DocumentComponent::NORMALIZE_FILTERS.each do |klass|
+      context "when #{klass.name} checks the result's validity" do
+        let(:filter) { klass.new }
+        it { is_expected.to satisfy(&filter.method(:valid?)) }
       end
     end
   end
